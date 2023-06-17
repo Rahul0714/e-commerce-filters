188b9f95ec9bbd9826c9682dcf65632857a96296
Rahul Mujumdar <rahulmujumdar2000@gmail.com>
2023-06-17 20:16:16 +0530

    done design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Navigation from "./navigation/Nav";
+import Products from "./products/Products";
+import Recommended from "./Recommended/Recommended";
 import "./App.css";
+import Sidebar from "./Sidebar/Sidebar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Sidebar />
+      <Navigation />
+      <Recommended />
+      <Products />
+    </>
+  );
 }
 
 export default App;
